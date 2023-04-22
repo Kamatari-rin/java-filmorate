1fc71389933319c6368c5a02089b37afd60fa1cb
Kamatari-rin <kamatarisama@gmail.com>
2023-04-22 23:52:26 +0500

    first commit

---
+++ java-filmorate_1/src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private Long id;
+    @Email(message = "Incorrect email.")
+    @NotNull(message = "email cannot be empty.")
+    private String email;
+    @NotBlank(message = "Name cannot be empty.")
+    private String login;
+    private String name;
+    private LocalDate birthday;
+
+    public User(String email, String login, String name, LocalDate birthday) {
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
+}
