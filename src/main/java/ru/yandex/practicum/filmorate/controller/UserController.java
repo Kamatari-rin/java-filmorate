ccdab8c7a7f2a1f9870eeee8a92da1877340479c
Kamatari-rin <kamatarisama@gmail.com>
2023-04-25 19:33:27 +0500

    Users and Films controllers update

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.CustomRestExceptionHandler;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.ArrayList;
