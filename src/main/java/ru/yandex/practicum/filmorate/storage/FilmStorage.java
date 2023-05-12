eeec3df9b0d11c259a69853d9291236d4270ae4f
Kamatari-rin <kamatarisama@gmail.com>
2023-05-13 00:05:02 +0500

    Sprint 10 all done

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Map;
 
 public interface FilmStorage {
+
     Film update(Film film);
     Film add(Film film);
     Map<Long, Film> getFilmsMap();
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.Map;
 
 public interface UserStorage {
+
     Map<Long, User> getUsersMap();
     User create(User user);
     User update(User user);
